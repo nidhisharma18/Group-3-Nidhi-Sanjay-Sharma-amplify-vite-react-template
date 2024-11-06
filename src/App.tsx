caec69f281f54019b56d5d3535bd338215a761cd
Nidhi Sharma <nidhisharma@Needys-MacBook.local>
2024-11-06 16:04:52 -0500

    added authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
+import { useAuthenticator } from '@aws-amplify/ui-react';
+  
+function deleteTodo(id: string) {
+  client.models.Todo.delete({ id })
+}
 
 const client = generateClient<Schema>();
 
 function App() {
+  const { signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
@@ -33,6 +39,7 @@ function App() {
           Review next step of this tutorial.
         </a>
       </div>
+      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
